f90088a74dffe8cbc541a827efbd98090fe038db
godole <godole199@naver.com>
2020-01-17 16:33:14 +0900

    Save200115

---
+++ MotorBall/Assets/Scripts/Client/WeaponParser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+public class WeaponParser
+{
+    XmlDocument m_WeaponDocument;
+    
+    WeaponParser(string filename)
+    {
+        m_WeaponDocument = XMLManager.LoadXML(filename);
+    }
+}
